f504f1faf174726abb44d549f855baa77c71167e
YCL <121530406+YCL0609@users.noreply.github.com>
2023-08-21 18:02:49 +0800

    Add files via upload

--- models/三月七/index.js
+++ models/三月七/index.js
@@ -10,88 +10,80 @@ import { MMDAnimationHelper } from 'three/addons/animation/MMDAnimationHelper.js
 let stats;
 let mesh, camera, scene, renderer, effect, composer;
 let helper, physicsHelper;
+
 const scriptURL = import.meta.url;
 const scriptPath = scriptURL.substring(0, scriptURL.lastIndexOf('/') + 1);
-const file = scriptPath + 'index.pmx';
+const file = `${scriptPath}index.pmx`;
 const clock = new THREE.Clock();
-const gammaValue = 1.0; // 设置伽马值的大小，可根据需要进行调整
+const gammaValue = 1.0;
 
 Ammo().then(function (AmmoLib) {
-    Ammo = AmmoLib;
-    init(file);
-    animate();
+  Ammo = AmmoLib;
+  init(file);
+  animate();
 });
 
 function init(file) {
-    const container = document.createElement('div');
-    document.body.appendChild(container);
+  const container = document.createElement('div');
+  document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 30;
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+  camera.position.z = 30;
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000);
 
-    const gridHelper = new THREE.PolarGridHelper(5, 8, 3);
-    gridHelper.position.y = -10;
-    scene.add(gridHelper);
+  const gridHelper = new THREE.PolarGridHelper(5, 8, 3);
+  gridHelper.position.y = -10;
+  scene.add(gridHelper);
 
-    const ambient = new THREE.AmbientLight(0xe6e6e6, 3);
-    scene.add(ambient);
+  const ambient = new THREE.AmbientLight(0xe6e6e6, 3);
+  scene.add(ambient);
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // 创建 Effect Composer
-    composer = new EffectComposer(renderer);
-    composer.addPass(new RenderPass(scene, camera));
+  composer = new EffectComposer(renderer);
+  composer.addPass(new RenderPass(scene, camera));
 
-    effect = new OutlineEffect(renderer);
+  effect = new OutlineEffect(renderer);
 
-    stats = new Stats();
-    container.appendChild(renderer.domElement);
-    container.appendChild(stats.dom);
+  stats = new Stats();
+  container.appendChild(renderer.domElement);
+  container.appendChild(stats.dom);
 
-    helper = new MMDAnimationHelper({
-        afterglow: 2.0,
-    });
+  helper = new MMDAnimationHelper({
+    afterglow: 2.0,
+  });
 
-    const loader = new MMDLoader();
+  const loader = new MMDLoader();
+  loader.load(file, function (mesh) {
+    mesh.position.y = -10;
+    scene.add(mesh);
+  });
 
-    loader.load(
-        file,
-        function (mesh) {
-            mesh.position.y = -10;
-            scene.add(mesh);
-        },
-    );
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 10;
+  controls.maxDistance = 100;
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.minDistance = 10;
-    controls.maxDistance = 100;
-
-    window.addEventListener('resize', onWindowResize);
+  window.addEventListener('resize', onWindowResize);
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    effect.setSize(window.innerWidth, window.innerHeight);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    composer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  effect.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  composer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-    requestAnimationFrame(animate);
-
-    stats.begin();
-    helper.update(clock.getDelta());
-
-    // 使用 Effect Composer 渲染场景，并将结果通过伽马值调整后输出
-    composer.render();
-    renderer.toneMappingExposure = Math.pow(gammaValue, 1.0); // 根据伽马值计算曝光度
-    renderer.render(scene, camera);
-
-    stats.end();
+  requestAnimationFrame(animate);
+  stats.begin();
+  helper.update(clock.getDelta());
+  composer.render();
+  renderer.toneMappingExposure = Math.pow(gammaValue, 1.0);
+  renderer.render(scene, camera);
+  stats.end();
 }
